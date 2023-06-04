3f36c6d2b292b0a4c49c94ad839eba46c5303195
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-04 14:11:30 +0200

    backend

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -21,4 +25,4 @@ mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: tru
     .then(() => app.listen(PORT, () => {console.log(`Server Running on Port: http://localhost:${PORT}`)}))
     .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+   // is no longer state of the art  mongoose.set('useFindAndModify', false);
