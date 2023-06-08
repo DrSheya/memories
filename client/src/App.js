be8e8212f12745c00c05fce7df76b1e20a174803
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-08 22:22:13 +0200

    adding components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { Component } from 'react';
+import Form from './components/Form/Form.js';
+import Posts from './components/Posts/Posts.js';
+import Post from './components/Posts/Post/Post.js';
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import memories from './images/memories.png';
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" width="70" ></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom';
 
 import App from './App';
 
-ReactDom.render (<App/>, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App />);
