93e7978815ce2419fc2842cb11627458ba748a47
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-11 01:29:29 +0200

    creating form

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
+app.use('/posts', postRoutes);
 
 // https://mongodb/database/atlas
 
 const CONNECTION_URL = 'mongodb+srv://fullstackmern:fullstackmern123@sheya-00.dewczxi.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {console.log(`Server Running on Port: http://localhost:${PORT}`)}))
