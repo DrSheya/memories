bd78990735a1a8f30d1c89bc17bad411b072f0de
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-11 00:22:25 +0200

    update redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 
+import { getPosts } from './actions/posts';
 import Form from './components/Form/Form.jsx';
 import Posts from './components/Posts/Posts.jsx';
 import memories from './images/memories.png';
@@ -11,6 +12,12 @@ import useStyles from './styles.jsx';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(getPosts());
+    }, [dispatch]);
+
 
     return (
         <Container maxidth="lg">
--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Post from './Post/Post';
+import { useSelector } from 'react-redux';
 import useStyles from './styles.jsx';
 
 const Posts = () => {
+    const posts = useSelector((state)=> state.posts);
     const classes = useStyles();
+
+
+    console.log (posts);
     return (
         <>
             <h1>Posts</h1>
