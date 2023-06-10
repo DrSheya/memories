76335569442a827f44c72f88c982ee6d367e5146
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-10 23:17:17 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 
 import Form from './components/Form/Form.jsx';
