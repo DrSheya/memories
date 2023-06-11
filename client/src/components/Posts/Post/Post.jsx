f1a688d20c2d708011948200026f32f6e4255e78
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-11 17:50:54 +0200

    update functionality

--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -5,8 +5,10 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from "react-redux";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
+    const dispatch = useDispatch();
     const classes = useStyles();
     return (
         <Card className={classes.card}>
@@ -22,7 +24,7 @@ const Post = ({ post }) => {
 
             <div className={classes.overlay2}>
 
-                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
 
                     <MoreHorizIcon fontSize= "medium" />
 
@@ -35,10 +37,10 @@ const Post = ({ post }) => {
                 <Typography variant="body2" color="textSecondary"> {post.tags.map((tag) => `#${tag} `)} </Typography>
                 
             </div>
-
+                <Typography className={classes.title} variant="h5" gutterBottom> { post.title } </Typography>
             <CardContent>
-
-                <Typography className={classes.title} variant="h5" gutterBottom> { post.message } </Typography>
+            
+                <Typography  variant="h5" gutterBottom> { post.message } </Typography>
 
             </CardContent>
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 
@@ -29,3 +30,16 @@ export const createPost = async (req, res) => {
     }
 }
 
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
