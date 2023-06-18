484a0facdc4654afa34bb46d65f2c2f8d033a63e
Sheya <115377434+DrSheya@users.noreply.github.com>
2023-06-18 22:56:25 +0200

    a

--- client/src/actions/auth.jsx
+++ client/src/actions/auth.jsx
@@ -1,21 +1,37 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
+import { useNavigate } from 'react-router-dom';
 
-export const signin = (formData, history) => async (dispatch) => {
+export const signin = (formData) => async (dispatch) => {
   try {
-    
 
-    history.push('/');
+    // sign in the user
+
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
+    const navigate = useNavigate();
+
+    navigate('/');
   } catch (error) {
     console.log(error);
   }
 };
 
-export const signup = (formData, history) => async (dispatch) => {
+export const signup = (formData) => async (dispatch) => {
   try {
 
+    // sing up the user
 
-    history.push('/');
+
+    const { data } = await api.signUp(formData);
+
+    const navigate = useNavigate();
+
+    dispatch({ type: AUTH, data });
+
+    navigate('/');
   } catch (error) {
     console.log(error);
   }
